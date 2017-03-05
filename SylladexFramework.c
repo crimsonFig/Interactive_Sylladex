d5b8daa39fb542dbc140f602d79b209a9f781159
Tris Scal <triss1@icloud.com>
2017-03-05 02:10:04 -0600

    Initial Commit
    Decided to upload to GitHub, this is the initial commit of my project
    so far.

---
+++ SylladexFramework.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+--- Modus by Triston Scallan
+Purpose:
+    A personal project that is an interactive inventory management
+    datastructure for entertainment purposes, based on Homestuck webcomic
+    inventory modi. Meant to be controlled through a console.
+    ---
+Command Parameters:
+    n/a
+Input:
+    An optional text file to be read and load as a modus.
+Results: N/A
+Returns: N/A
+Notes:
+*******************************************************************************/
+// If compiling using visual studio, tell the compiler not to give its warnings
+// about the safety of scanf and printf
+#define _CRT_SECURE_NO_WARNINGS 1
+
+// Include files
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/*/Fetch Modus Files to include
+#include "PentaFile.h"
+#include "NodeMap.h"
+#include "timeBox.h"
+#include "tarotDeck.h"
+*/
---
+++ pentaFile.c
@@ -0,0 +1,394 @@
+/******************************************************************************
+PentaFile PFModus by Triston Scallan
+Purpose:
+    A personal project that is an interactive inventory management
+    datastructure for entertainment purposes, based on Homestuck webcomic
+    inventory modi. Meant to be controlled through a console.
+    ---
+    This fetch modus is the PentaFile pfModus, a modus designed for use with a sylladex.
+    Using a struct comprised of 5 arrays containing 5 cards, and busting if a
+    single array is overfilled.
+    This is likened to a File Cabinet. 5 folders that can hold 5 files each.
+Command Parameters:
+    n/a
+Input:
+    An optional text file to be read and load as a modus.
+Results: N/A
+Returns: N/A
+Notes:
+    1. The inventory only holds 25 cards, 5 cards in 5 folders.
+    2. If 6 cards are placed into a folder, 5 are ejected and the 6th is pushed
+    3. The current pfModus can have all its information saved and loaded to a txt file.
+*******************************************************************************/
+// If compiling using visual studio, tell the compiler not to give its warnings
+// about the safety of scanf and printf
+#define _CRT_SECURE_NO_WARNINGS 1
+
+// Include files
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+//#include "cs2123p0.h"
+
+/////Defined constants
+#define TRUE 1
+#define SUCCESS 5
+#define FALSE 0
+#define NAMESIZE 12
+#define EMPTY "55555"
+/////Prototypes
+PFModus newPFModus()                                            ;
+void save(PFModus pfModus)                                      ;
+PFModus load(char fileName)                                     ;
+Card takeOutByIndex(PFModus pfModus, Card type[], int index)    ;
+Card takeOutByName(PFModus pfModus, Card type[], char value)    ;
+int push(PFModus pfModus, char type, char card)                 ;
+int isFull(Card card[])                                         ;
+int forceEject(Card card[])                                     ;
+void drawInventory(PFModus pfModus)                             ;
+/////structs and typedefs
+typedef int Data;                   //simply renamed
+typedef struct
+{
+    char item[NAMESIZE + 1];    //12 for an card name. item title
+    char captchaCode[8];        //7 long captcha code. Code for w/e
+    int  inUse;                 //used to track if the card is empty or filled
+} Card;
+typedef struct
+{
+ //char note[64];      //personal note. NOT NEEDED.
+ //Data stats[25];     //stats to be tracked go here, might output on save. might move this to the sylladex instead.
+ //Data index[25];     //boolean index of the inventory, TRUE if filled, FALSE if that slot is empty. replaced by card->inUse
+    //the next 5 array's can hold card of a certain kind. These are 5 arrays of the Card type, a card is a defined string.
+    //consider a 2d array.
+    Card weapons[5]    ;
+    Card survival[5]   ;
+    Card misc[5]       ;
+    Card info[5]       ;
+    Card keyCritical[5];
+} PFModusImp;             //the actual inventory structure pfModus
+typedef PFModusImp *PFModus;//a pointer to the pfModus implementation
+typedef struct
+{
+    int isEmpty = TRUE;
+    Card heldItem;
+} Hand;
+////functions
+
+>>"replace all pfModus->index with pfModus->card.inUse, and recode those bits"
+>>"replace all pfModus->card or 'card' with pfModus->card.item or 'card.item'"
+>>"include an eject all function"
+>>"rename functions to include a pf- prefix"
+
+/*************************** main ***********************************
+********************************************************************/
+int main(int argc, char *argv[])
+{
+    PFModus pfModus = newPFModus();
+    Hand hand;
+    push(pfModus, "weapons", "Dmnd Staff");
+    push(pfModus, "weapons", "Wand");
+    push(pfModus, "weapons", "Blade");
+    push(pfModus, "weapons", "Marbles");
+    drawInventory(pfModus);
+    push(pfModus, "misc", "Incense");
+    push(pfModus, "info", "101101010");
+    push(pfModus, "survival", "Gem");
+    drawInventory(pfModus);
+    push(pfModus, "weapons", "SynOrb");
+    push(pfModus, "weapons", "Lotus Blade");
+    drawInventory(pfModus);
+    strcpy(hand.heldItem, takeOutByName(pfModus, pfModus.weapons, "Lotus Blade"));
+    printf("%s\n", hand.heldItem);
+    strcpy(hand.heldItem, takeOutByName(pfModus, pfModus.survival, 1));
+    printf("%s\n", hand.heldItem);
+    drawInventory(pfModus);
+}
+
+/*************************** newCard *********************************///done
+Card newCard()
+{
+    char empty = EMPTY;
+    Card card = {empty, "0000000", FALSE};
+    return card;
+}
+/*************************** newPFModus ******************************///done
+PFModus newPFModus()
+{
+    int i;
+    char empty = EMPTY;
+    Card newCard = newCard();
+    PFModus pfModus = (PFModus) malloc(sizeof(PFModusImp));
+
+    for (i = 0; i < 5; i++)
+    {
+        pfModus.weapons[i] = newCard;
+        pfModus.survival[i] = newCard;
+        pfModus.misc[i] = newCard;
+        pfModus.info[i] = newCard;
+        pfModus.keyCritical[i] = newCard;
+    }
+    return pfModus;
+}
+
+/*************************** save ***********************************
+i need to follow a standardized way of saving to a file and reading from a txt
+file so that all modus are compatible. i need to figure out if i should create
+a seperate header file that handle this or if each modus should contain the load
+and saving code for that particular modus but that it follows a standard output
+format. At best, i would have to create a hybrid where the modus uses the header
+file to access generalized functions to output. My current solution would to
+create functions for save and load and see if i can modularize them to my given
+standard, and then since they'd be a common code functions, i can put them in an
+include header file and have each modus be able to utilize those functions to
+process it's current set of cards. >>fgets.713 overview of c for stdin.
+********************************************************************/
+void save(PFModus pfModus)
+{
+
+}
+
+/*************************** load ***********************************
+********************************************************************/
+PFModus load(char fileName)
+{
+
+}
+
+/*************************** takeOutByIndex *************************
+********************************************************************/
+Card takeOutByIndex(PFModus pfModus, Card folder[], int fileIndex)
+{
+    Card card = newCard();
+    if (fileIndex < 0 || fileIndex > 5)
+    {
+        printf("%s\n", "requested index out of bound. Returned an EMPTY card");
+        return card;
+    }
+    //Pass the card out of the array so that it is now empty, and return said card to user
+    card = folder[fileIndex];           //set card = to specified card
+    folder[fileIndex] = newCard();      //replace the card in inv with an empty card
+    return card;
+}
+
+/*************************** takeOutByName **************************
+********************************************************************/
+Card takeOutByName(PFModus pfModus, Card folder[], char fileIndex)
+{
+    Card card = newCard();
+    int bFound = FALSE;
+    int index;
+    //search for value
+    for (i = 0, i < 5, i++)
+    {
+        if (strcmp(type[i], value) == 0) //card found!
+        {
+            strcpy(card, type[i]);
+            strcpy(type[i], empty);
+            bFound = TRUE;
+            index = i;
+            printf("%s %s\n","found the requested card: ", card);
+            break;
+        }
+    }
+    if (!bFound)
+    {
+        printf("%s\n", "Could not find requested card, Returned an EMPTY card");
+        return card;
+    }
+    //figure out which type "type[]" is so that the pfModus index can be set to FALSE
+    //compare the address of the passed type to one of the defined types
+    if ((pfModus->weapons) == type){
+        pfModus->index[0 + index] = FALSE;
+    }
+    else if ((pfModus->survival) == type){
+        pfModus->index[5 + index] = FALSE;
+    }
+    else if ((pfModus->misc) == type){
+        pfModus->index[10 + index] = FALSE;
+    }
+    else if ((pfModus->info) == type){
+        pfModus->index[15 + index] = FALSE;
+    }
+    else if ((pfModus->keyCritical) == type){
+        pfModus->index[20 + index] = FALSE;
+    }
+    else{
+        printf("%s\n", "An error occured in setting the index to FALSE");
+    }
+    return card;
+}
+
+/*************************** push **********************************///done
+int push(PFModus pfModus, char type, char card)
+{
+    int i;
+    if (sizeof(card) > (NAMESIZE + 1))
+    {
+        printf("%s\n", "you've named said object too long, max is 12 letters.");
+        return FALSE;
+    }
+    //using the first letter of the type as a key, we insert the card into the pfModus
+    switch (type[0]) {
+        case 'w'/*eapons*/:
+            i = 0;
+            if (pfModus->index[i+4] == TRUE)  //if the 5th object in the array is filled, dump the array and push to 1st element
+            {
+                forceEject(pfModus->weapons);
+                pfModus->index[i] = TRUE;
+                strcpy(pfModus->weapons[i], card);
+                return SUCCESS;
+            }
+            for (i = 0; i < 5; i++)         //search for the first empty slot and push to that, then set that slot to filled
+            {
+                if (pfModus->index[i]) //if true, skip to the next index
+                    continue;
+                pfModus->index[i] = TRUE; //found a slot with FALSE, pushing card to that slot
+                strcpy(pfModus->weapons[i], card);
+                return SUCCESS;
+            }
+            printf("%s\n", "unable to push card");
+            break;
+        case 's'/*urvival*/:
+            i = 5;
+            if (pfModus->index[i+4] == TRUE)  //if the 5th object in the array is filled, dump the array and push to 1st element
+            {
+                forceEject(pfModus->survival);
+                pfModus->index[i] = TRUE;
+                strcpy(pfModus->survival[i%5], card);
+                return SUCCESS;
+            }
+            for (i = 5; i < 10; i++)         //search for the first empty slot and push to that, then set that slot to filled
+            {
+                if (pfModus->index[i]) //if true, skip to the next index
+                    continue;
+                pfModus->index[i] = TRUE; //found a slot with FALSE, pushing card to that slot
+                strcpy(pfModus->survival[i%5], card);
+                return SUCCESS;
+            }
+            printf("%s\n", "unable to push card");
+            break;
+        case 'm'/*isc*/:
+            i = 10;
+            if (pfModus->index[i+4] == TRUE)  //if the 5th object in the array is filled, dump the array and push to 1st element
+            {
+                forceEject(pfModus->misc);
+                pfModus->index[i] = TRUE;
+                strcpy(pfModus->misc[i%5], card);
+                return SUCCESS;
+            }
+            for (i = 10; i < 15; i++)         //search for the first empty slot and push to that, then set that slot to filled
+            {
+                if (pfModus->index[i]) //if true, skip to the next index
+                    continue;
+                pfModus->index[i] = TRUE; //found a slot with FALSE, pushing card to that slot
+                strcpy(pfModus->misc[i%5], card);
+                return SUCCESS;
+            }
+            printf("%s\n", "unable to push card");
+            break;
+        case 'i'/*nfo*/:
+            i = 15;
+            if (pfModus->index[i+4] == TRUE)  //if the 5th object in the array is filled, dump the array and push to 1st element
+            {
+                forceEject(pfModus->info);
+                pfModus->index[i] = TRUE;
+                strcpy(pfModus->info[i%5], card);
+                return SUCCESS;
+            }
+            for (i = 15; i < 20; i++)         //search for the first empty slot and push to that, then set that slot to filled
+            {
+                if (pfModus->index[i]) //if true, skip to the next index
+                    continue;
+                pfModus->index[i] = TRUE; //found a slot with FALSE, pushing card to that slot
+                strcpy(pfModus->info[i%5], card);
+                return SUCCESS;
+            }
+            printf("%s\n", "unable to push card");
+            break;
+        case 'k'/*ey card*/:
+            i = 20;
+            if (pfModus->index[i+4] == TRUE)  //if the 5th object in the array is filled, dump the array and push to 1st element
+            {
+                forceEject(pfModus->keyCritical);
+                pfModus->index[i] = TRUE;
+                strcpy(pfModus->keyCritical[i%5], card);
+                return SUCCESS;
+            }
+            for (i = 20; i < 25; i++)         //search for the first empty slot and push to that, then set that slot to filled
+            {
+                if (pfModus->index[i]) //if true, skip to the next index
+                    continue;
+                pfModus->index[i] = TRUE; //found a slot with FALSE, pushing card to that slot
+                strcpy(pfModus->keyCritical[i%5], card);
+                return SUCCESS;
+            }
+            printf("%s\n", "unable to push card");
+            break;
+        default:
+            printf("%s\n", "Not a valid card type");
+            return FALSE;
+    }
+}
+
+/*************************** isFull ********************************///done
+int isFull(Card card[])
+{
+    char empty = EMPTY;
+    return abs(strcmp(card[4],empty)); //if it equals EMPTY, then it will return 0(FALSE), otherwise it will return TRUE
+}
+
+/*************************** forceEject ****************************///done
+int forceEject(Card card[])
+{
+    int i;
+    char empty = EMPTY;
+    Card toEject;
+    for (i = 0; i < 5; i++) //dumps the entire contents of the card array/folder
+    {
+        toEject = card[i];
+        strcpy(card[i], empty);
+        //toEject now releases the card to the world from the pfModus.
+    }
+}
+
+/*************************** drawInventory *************************///done
+void drawInventory(PFModus pfModus)
+{
+    int card;
+    printf("----------------------------------------------------------------------------\n");
+
+    for (card = 0; card < 5; card++)
+    {
+        printf("| %12s ", pfModus->weapons[card]);
+    }
+    printf("|\n");
+
+    for (card = 0; card < 5; card++)
+    {
+        printf("| %12s ", pfModus->food[card]);
+    }
+    printf("|\n");
+
+    for (card = 0; card < 5; card++)
+    {
+        printf("| %12s ", pfModus->misc[card]);
+    }
+    printf("|\n");
+
+    for (card = 0; card < 5; card++)
+    {
+        printf("| %12i ", pfModus->info[card]);
+    }
+    printf("|\n");
+
+    for (card = 0; card < 5; card++)
+    {
+        printf("| %12s ", pfModus->keyCritical[card]);
+    }
+    printf("|\n");
+
+    printf("----------------------------------------------------------------------------\n\n");
+}
