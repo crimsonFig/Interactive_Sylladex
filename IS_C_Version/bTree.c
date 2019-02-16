d8cab5c3f3155eba0402adf80ce6d9f2dfdbba7d
Triston Scallan <triston.scaln@icloud.com>
2019-02-15 20:45:54 -0600

    remove deprecated C version

--- IS_C_Version/bTree.c
+++
@@ -1,9 +0,0 @@
-/* a modus that functions as a self balancing binary tree. can only safely
-    pop from the leaves, popping an item will also pop all subtree items.
-    this means popping the root will cause the entire tree to be popped.
-    inserting an item will happen as a leaf and can trigger a tree rebalancing.
-    the tree will use a struct and pointers to nodes.
-
-the uniqueness of this modus is that it challenges the user to insert and pop
-    items to rebalance the tree to where the items they want become more
-    accessable (rebalance the tree to move the root to a further subtree)
--- IS_C_Version/heap.c
+++
@@ -1,3 +0,0 @@
-//modus functions as an array heap, [a,[b,c],[d,e,f,g]]. may rebalance itself.
-//acts like the bTree combined with the Array modus, like an upgrade?
-//maybe just be an actual heap.
